ef0a210e95ee2a480120b26b212011803c02ef47
Massimiliano Patrizi <40141221+MassPatrizi@users.noreply.github.com>
2019-07-18 15:37:52 +0200

    v5.0
    -Correzione schermata 'Credits'

--- credits.lua
+++ credits.lua
@@ -30,33 +30,33 @@ function scene:create( event )
     background.x = display.contentCenterX
 	background.y = display.contentCenterY
 
-    local y = 420;
+    local y = 300;
 
-    local title = display.newText( sceneGroup, "CREDITS", display.contentCenterX, y - 220, "Riffic.ttf", 60 )
+    local title = display.newText( sceneGroup, "CREDITS", display.contentCenterX, y - 100, "Riffic.ttf", 60 )
     title:setFillColor( 0.75, 0.78, 1 )
 
     local credits01 = display.newText( sceneGroup, "DEVELOPED BY", display.contentCenterX, y , "Riffic.ttf", 22 )
     credits01:setFillColor(0.75, 0.78, 1 )
 
 	local credits1 = display.newText( sceneGroup, "Cristian Tinaburri", display.contentCenterX, y + 60, "Riffic.ttf", 35 )
-	credits1:setFillColor( 0.75, 0.78, 1 )
-	
-	local credits1A = display.newText( sceneGroup, "[Lead Dev]", display.contentCenterX, y + 90, "Riffic.ttf", 22 )
-	credits1A:setFillColor( 0.75, 0.78, 1 )
+	credits1:setFillColor( 255, 255, 255 )
 
-    local credits2 = display.newText( sceneGroup, "Giorgia Nesci", display.contentCenterX, y + 160, "Riffic.ttf", 35 )
-    credits2:setFillColor( 0.75, 0.78, 1 )
-
-	local credits2A = display.newText( sceneGroup, "[Audio - Coder]", display.contentCenterX, y + 190, "Riffic.ttf", 22 )
-	credits2A:setFillColor( 0.75, 0.78, 1 )
+    local credits2 = display.newText( sceneGroup, "Giorgia Nesci", display.contentCenterX, y + 120, "Riffic.ttf", 35 )
+    credits2:setFillColor( 255, 255, 255 )
 	
-    local credits3 = display.newText( sceneGroup, "Massimiliano Patrizi", display.contentCenterX, y + 260, "Riffic.ttf", 35 )
-	credits3:setFillColor( 0.75, 0.78, 1 )
-	
-	local credits3A = display.newText( sceneGroup, "[Designer - Coder]", display.contentCenterX, y + 290, "Riffic.ttf", 22 )
-    credits3A:setFillColor( 0.75, 0.78, 1 )
+    local credits3 = display.newText( sceneGroup, "Massimiliano Patrizi", display.contentCenterX, y + 180, "Riffic.ttf", 35 )
+	credits3:setFillColor( 255, 255, 255 )
 
-	local menuButton = display.newText( sceneGroup, "Menu", display.contentCenterX, y + 380, "Riffic.ttf", 44 )
+	local musicCredit = display.newText( sceneGroup, "MUSIC", display.contentCenterX, y + 250, "Riffic.ttf", 22 )
+	musicCredit:setFillColor( 0.75, 0.78, 1 )
+	
+	local music1 = display.newText( sceneGroup, "8 Bit Space Groove! by HeatleyBros", display.contentCenterX, y + 300, "Riffic.ttf", 22 )
+	music1:setFillColor( 255, 255, 255 )
+
+	local music2 = display.newText( sceneGroup, "[8-Bit Speedcore] antiPLUR - Speed of Link", display.contentCenterX, y + 350, "Riffic.ttf", 22 )
+	music2:setFillColor( 255, 255, 255 )
+
+	local menuButton = display.newText( sceneGroup, "Menu", display.contentCenterX, y + 500, "Riffic.ttf", 44 )
     menuButton:setFillColor( 0.75, 0.78, 1 )
 	menuButton:addEventListener( "tap", gotoMenu )
 	
